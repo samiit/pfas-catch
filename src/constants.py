e39ca7b61d0a3d34123669440d50d3c7faac717f
Sam Mathew <sam.cfd.iitm@gmail.com>
2025-07-19 19:17:35 +0200

    Get all images for rendering

--- src/constants.py
+++ src/constants.py
@@ -9,8 +9,23 @@ class Molecule(BaseModel):
     image: str
 
 
+pfba_molecule = Molecule(
+    name="per-fluoro butanoic acid",
+    smiles="O=C(O)C(F)(F)C(F)(F)C(F)(F)F",
+    image="PFOA",
+)
+pfbs_molecule = Molecule(
+    name="per-fluoro butane sulfonic acid",
+    smiles="O=S(C(F)(F)C(F)(F)C(F)(F)C(F)(F)F)(O)=O",
+    image="PFBS",
+)
 pfoa_molecule = Molecule(
     name="per-fluoro butanoic acid",
     smiles="O=C(O)C(F)(F)C(F)(F)C(C(F)(F)C(F)(F)C(C(F)(F)F)(F)F)(F)F",
     image="PFOA",
+)
+pfos_molecule = Molecule(
+    name="per-fluoro octane sulfonic acid",
+    smiles="O=S(C(F)(F)C(F)(F)C(F)(F)C(C(F)(F)C(F)(F)C(F)(F)C(F)(F)F)(F)F)(O)=O",
+    image="PFOS",
 )
