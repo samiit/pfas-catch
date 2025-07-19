a75b22b4b234cbef9a145f7412fc8f6764abee6a
Sam Mathew <sam.cfd.iitm@gmail.com>
2025-07-19 19:02:19 +0200

    Better data management

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,16 @@
 """Constants for the repository"""
 
-pfoa_name = "per-fluoro butanoic acid"
-pfoa_smiles = "O=C(O)C(F)(F)C(F)(F)C(C(F)(F)C(F)(F)C(C(F)(F)F)(F)F)(F)F"
-images_dict = {pfoa_smiles: "PFOA"}
+from pydantic import BaseModel
+
+
+class Molecule(BaseModel):
+    name: str
+    smiles: str
+    image: str
+
+
+pfoa_molecule = Molecule(
+    name="per-fluoro butanoic acid",
+    smiles="O=C(O)C(F)(F)C(F)(F)C(C(F)(F)C(F)(F)C(C(F)(F)F)(F)F)(F)F",
+    image="PFOA",
+)
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 """Utilities for the PFAS catch/match application"""
 
-from src.constants import images_dict, pfoa_smiles, pfoa_name
+from src.constants import pfoa_molecule
 
 
 def get_molecules(text: str) -> list:
     """Uses LLM to get the molecule name out of a given text description"""
-    molecule_name = [pfoa_name]  # !!!
+    molecule_name = [pfoa_molecule.name]  # !!!
     return molecule_name
 
 
@@ -13,10 +13,10 @@ def get_smiles(molecule_name: str) -> str:
     """
     Uses LLM to match to the right SMILES representation of the molecule name
     """
-    smiles_name = pfoa_smiles  # !!!
+    smiles_name = pfoa_molecule.smiles  # !!!
     return smiles_name
 
 
 def get_filename(smiles: str):
     """Get the filename from a dictionary"""
-    return images_dict.get(smiles, "default_image")
+    return pfoa_molecule.image
